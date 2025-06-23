e8dc546d18594a25ef2a06138183c7b8d9d0a5fb
Danny Noonan <daniel.christopher.noonan@cern.ch>
2025-06-23 11:35:11 -0500

    fix timestamp from replacing bent pins

--- utils/shipments_7_8_9.py
+++ utils/shipments_7_8_9.py
@@ -4,18 +4,18 @@ from LocationsDB import LocationsDatabase
 from GradesDB import GradesDatabase
 
 #left commented out on git to avoid accidentally running, update path when needed
-loc_db = LocationsDatabase('../database_files/test_locations.db')
+loc_db = LocationsDatabase('../database_files/testing_database.db')
 grade_db = GradesDatabase('../database_files/test_grade_database.db')
 
 #moved three chips with bent pins back to initial location
-loc_db.setChipGrade(chip_id=1002285, grade="Reject", comments="Manual pin inspection", timestamp="2025-01-28 12:00:00")
-loc_db.rejectChip(chip_id=1002285, start_tray=18009, start_position=35, new_tray=10022, new_position=85, comments="Manual pin inspection", timestamp="2025-01-28 12:00:00")
+loc_db.setChipGrade(chip_id=1002285, grade="Reject", comments="Manual pin inspection", timestamp="2025-01-29 12:00:00")
+loc_db.rejectChip(chip_id=1002285, start_tray=18009, start_position=35, new_tray=10022, new_position=85, comments="Manual pin inspection", timestamp="2025-01-29 12:00:00")
 
-loc_db.setChipGrade(chip_id=1002309, grade="Reject", comments="Manual pin inspection", timestamp="2025-01-28 12:00:00")
-loc_db.rejectChip(chip_id=1002309, start_tray=18009, start_position=42, new_tray=10023, new_position=9, comments="Manual pin inspection", timestamp="2025-01-28 12:00:00")
+loc_db.setChipGrade(chip_id=1002309, grade="Reject", comments="Manual pin inspection", timestamp="2025-01-29 12:00:00")
+loc_db.rejectChip(chip_id=1002309, start_tray=18009, start_position=42, new_tray=10023, new_position=9, comments="Manual pin inspection", timestamp="2025-01-29 12:00:00")
 
-loc_db.setChipGrade(chip_id=1002413, grade="Reject", comments="Manual pin inspection", timestamp="2025-01-28 12:00:00")
-loc_db.rejectChip(chip_id=1002413, start_tray=18009, start_position=77, new_tray=10024, new_position=13, comments="Manual pin inspection", timestamp="2025-01-28 12:00:00")
+loc_db.setChipGrade(chip_id=1002413, grade="Reject", comments="Manual pin inspection", timestamp="2025-01-29 12:00:00")
+loc_db.rejectChip(chip_id=1002413, start_tray=18009, start_position=77, new_tray=10024, new_position=13, comments="Manual pin inspection", timestamp="2025-01-29 12:00:00")
 
 # Change location to be shipment destination
 loc_db.shipTraysAndGenerateUploadCSV(
